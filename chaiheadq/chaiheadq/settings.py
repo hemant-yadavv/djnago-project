cbfa1aee67f5f6675961631260aad4c0546d2047
Hemant <hemantyadav1137@gmail.com>
2024-07-25 13:05:24 +0530

    custom user registraion

--- chaiheadq/chaiheadq/settings.py
+++ chaiheadq/chaiheadq/settings.py
@@ -127,3 +127,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_URL = '/accounts/login'
+
+LOGIN_REDIRECT_URL = '/tweet/'
+LOGOUT_REDIRECT_URL = '/tweet/'
